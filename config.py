e533e0a3e9343dff565a40605f88d7a1d198781e
IsabelVentura <36337389+IsabelVentura@users.noreply.github.com>
2018-02-24 09:10:56 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import web
 
-db_host = 'localhosttviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_host = 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'wqv8nyode7nnl8yv'
 db_user = 'yo553nj9odgp9a1y'
 db_pw = 'ea8t3uha78f6bhmq'
